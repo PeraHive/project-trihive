a9c5c156588d45ea739eabf82475b0eec568edaa
Janth Yapa <34100523+janithcyapa@users.noreply.github.com>
2025-09-03 19:53:46 +0530

    FoxGlove Layout

--- shadow/launch/mavros.launch.py
+++ shadow/launch/mavros.launch.py
@@ -13,7 +13,7 @@ def make_mavros_node(ns, fcu_url, gcs_url, sys_id, tgt_sys_id):
             output='screen',
             parameters=[{
                 'fcu_url': fcu_url,
-                # 'gcs_url': gcs_url,
+                'gcs_url': gcs_url,
                 'system_id': sys_id,
                 'target_system_id': tgt_sys_id,
             }],
